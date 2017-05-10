c8a8d5d185d38d181bc3b20d617d159f326844f3
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-05-09 19:48:27 -0500

    added angular navbar

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,1 +1,14 @@
-console.log("working..");
+// console.log("working..");
+"use strict";
+
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", ($scope) => {
+  $scope.cat = "Meow";
+  $scope.navItems = [{name: "New Item"}, {name: "All Items"}, {name: "Logout"}];
+});
+
+app.controller("ItemCtrl", ($scope) => {
+  $scope.dog = "woof";
+
+});
