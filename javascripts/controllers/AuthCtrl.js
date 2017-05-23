0195fa232efab324fbbcf3ec0faac8a87cbc96d0
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-05-22 20:02:18 -0500

    added user

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,23 +1,41 @@
-app.controller("AuthCtrl", function($scope, FIREBASE_CONFIG, AuthFactory, UserFactory){
-  $scope.auth = {};
+app.controller("AuthCtrl", function($rootScope, $scope, $location, FIREBASE_CONFIG, AuthFactory, UserFactory){
+  $scope.auth = {
+    email: "a@a.com",
+    password: "123456"
+  };
 
-  $scope.loginUser = () => {
-
+  let logMeIn = () => {
+    AuthFactory.authenticate($scope.auth).then((userCreds) => {
+      console.log("user creds", userCreds);
+      return UserFactory.getUser(userCreds.uid);
+    }, (error) => {
+      console.log("auth error", error);
+    }).then((user) => {
+      $rootScope.user = user;
+      $location.url('/items/list');
+    }).catch((error) => {
+      console.log("get user error", error);
+    });
   };
 
   $scope.registerUser = () => {
 
     AuthFactory.registerWithEmail($scope.auth)
     .then((didRegister) => {
-      // console.log("registered", didRegister);
       $scope.auth.uid = didRegister.uid;
-      return UserFactory.addUser($scope.auth.uid);
+      return UserFactory.addUser($scope.auth);
     }, (error) => {
       console.log("register error", error);
     }).then((registerComplete) => {
-      console.log("registerComplete",registerComplete);
+      // console.log("registerComplete",registerComplete);
+      $location.url('/items/list');
     }, (error) => {
       console.log("error", error);
     });
   };
+
+  $scope.loginUser = () => {
+    logMeIn();
+  };
+
 });
